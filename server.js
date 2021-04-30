1abc61a3d8942729550668b18ba6b4aadef63c11
Christin Mette <67912026+chmette@users.noreply.github.com>
2021-04-30 15:10:24 +0200

    feat: Add server file - set up server (#2)
    approved and good to go - merged

---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+app.listen(PORT, () => {
+  console.log(`👍 Backend Server started at http://localhost:${PORT}`);
+});
+
+//Middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // frontend URL should be configurable => //* later to do
+    credentials: true, // allow cookies to be sent from frontend to us
+  })
+);
+app.use(cookieParser());
+
+//ROUTES;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//ERROR HANDLING
+app.use(function errorHandler(err, req, res, next) {
+  console.log("⛔ ", err);
+  res.status(err.status || 500).send({
+    error: {
+      message: err.message,
+    },
+  });
+});
