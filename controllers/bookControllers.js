be9eca86d80287e2312761d53127618ee7e83427
Luke Dowling <67188696+luke-dowling@users.noreply.github.com>
2021-05-11 13:57:27 +0200

    [add] custom error handler (#8)
    * [add] custom error handler
    
    * [add] custom error handling to book controllers
    
    * [mod] adapted the custom errors in book controller

--- controllers/bookControllers.js
+++ controllers/bookControllers.js
@@ -1,9 +1,19 @@
 const Book = require('../models/Book');
+const customError = require('../helpers/customErrorHandler');
+const mongoose = require('mongoose');
 
-exports.getBook = async (req, res) => {
+exports.getBook = async (req, res, next) => {
   const { id } = req.params;
-  let book = await Book.findById(id);
-  res.json(book);
+
+  try {
+    let book = await Book.findById(id);
+    res.json(book);
+  } catch (err) {
+    if (err instanceof mongoose.Error.CastError) {
+      next(customError(`Book with ID ${id} does not exist`, 400));
+    }
+    next(err);
+  }
 };
 
 exports.getBooks = async (req, res) => {
@@ -12,18 +22,20 @@ exports.getBooks = async (req, res) => {
 };
 
 exports.addBook = async (req, res, next) => {
+  //todo: create middleware to check the req.body
   const bookData = req.body;
 
   try {
     let newBook = await Book.create(bookData);
     res.json(newBook);
   } catch (err) {
-    console.log(err);
+    next(err);
   }
 };
 
 exports.updateBook = async (req, res, next) => {
   const { id } = req.params;
+  //todo : create middle ware to check req.body is okay
 
   try {
     const updatedBook = await Book.findByIdAndUpdate(id, req.body, {
@@ -31,6 +43,9 @@ exports.updateBook = async (req, res, next) => {
     });
     res.json(updatedBook);
   } catch (err) {
+    if (err instanceof mongoose.Error.CastError) {
+      next(customError(`Book with ID ${id} does not exist`, 400));
+    }
     next(err);
   }
 };
@@ -42,7 +57,9 @@ exports.deleteBook = async (req, res, next) => {
     let deletedBook = await Book.findByIdAndDelete(id);
     res.json(deletedBook);
   } catch (err) {
-    console.log(err);
+    if (err instanceof mongoose.Error.CastError) {
+      next(customError(`Book with ID ${id} does not exist`, 400));
+    }
     next(err);
   }
 };
@@ -50,8 +67,9 @@ exports.deleteBook = async (req, res, next) => {
 exports.addInterestedUser = async (req, res, next) => {
   const { userId, bookId } = req.body;
 
+  //todo : turn this check into a middleware
   if (!userId || !bookId) {
-    res.json('A user ID and a book ID must be provided.');
+    customError('A user ID and a book ID must be provided', 500);
     return;
   }
 
@@ -65,7 +83,6 @@ exports.addInterestedUser = async (req, res, next) => {
     );
     res.json(updatedInterestedUser);
   } catch (err) {
-    console.log(err);
     next(err);
   }
 };
