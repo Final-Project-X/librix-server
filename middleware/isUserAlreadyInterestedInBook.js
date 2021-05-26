a71d2d98dbee3f44f718f9d0085ddc3dbc7a42f3
Luke Dowling <lpdowling26@googlemail.com>
2021-05-26 11:28:09 +0200

    [mod] changed to check both the user and the book schema in the middleware

--- middleware/isUserAlreadyInterestedInBook.js
+++ middleware/isUserAlreadyInterestedInBook.js
@@ -9,12 +9,25 @@ exports.isUserAlreadyInterestedInBook = async (req, res, next) => {
   }
 
   const book = await Book.findById(bookId);
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return next(customError(`User with id: ${userId} does not exists`));
+  }
+
+  if (!book) {
+    return next(customError(`Book with id: ${bookId} does not exists`));
+  }
 
   const interestedUserArray = book.interestedUsers.filter(
     (id) => id.toString() === userId.toString()
   );
 
-  if (interestedUserArray.length > 0) {
+  const interestedBookArray = user.booksInterestedIn.filter(
+    (id) => id.toString() === bookId.toString()
+  );
+
+  if (interestedUserArray.length > 0 || interestedBookArray.length > 0) {
     return next(
       customError('This user is already interested in this book', 400)
     );
