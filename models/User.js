5648851a005be0c86f1579e16f0ee2d84bede620
Luke Dowling <67188696+luke-dowling@users.noreply.github.com>
2021-06-01 12:03:03 +0200

    [add] auth to necessary routes and updated the user schema and contro… (#35)
    * [add] auth to necessary routes and updated the user schema and controller
    
    * [mod] delete headers with token inside
    
    * [add] auth to every route

--- models/User.js
+++ models/User.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const UserSchema = new Schema(
   {
@@ -49,9 +53,45 @@ const UserSchema = new Schema(
   }
 );
 
+UserSchema.methods.hashPassword = function () {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = bcrypt.hashSync(user.password, 10);
+  }
+};
+
+UserSchema.methods.comparePasswords = function (pwPlain) {
+  const user = this;
+  return bcrypt.compareSync(pwPlain, user.password);
+};
+
+UserSchema.methods.generateToken = function () {
+  const user = this;
+  const payload = { _id: user._id };
+  const token = jwt.sign(payload, JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRY || '5h',
+  });
+
+  return token;
+};
+
+UserSchema.statics.verifyToken = function (token) {
+  return jwt.verify(token, JWT_SECRET);
+};
+
+UserSchema.statics.findByToken = function (token) {
+  const User = this;
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (error) {
+    return;
+  }
+
+  return User.findById(payload._id);
+};
+
 const User = model('User', UserSchema);
 
 module.exports = User;
-
-//* MATCH
-// booksInterestedIn.owner === user.booksToOffer.interestedUsers => add new match
