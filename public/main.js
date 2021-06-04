cd26482c6a085f8f352893aa3df3b8a2cd731635
Luke Dowling <lpdowling26@googlemail.com>
2021-06-04 11:22:43 +0200

    [mod] updated the documentation

--- public/main.js
+++ public/main.js
@@ -1,1 +1,32 @@
 const userSchema = document.getElementById('user-schema');
+const codeArrOne = [
+  `"username": "username",`,
+  `"email": "email",`,
+  `"passowrd": "password",`,
+  `"city": "city",`,
+];
+const codeArrOneOptional = [
+  `"aboutMe": "aboutMe",`,
+  `"avatar": png / defualt is provided,`,
+  `"points": number / defualt 0,`,
+  `"booksToOffer": [objectIDs],`,
+  `"booksToRemember": [objectIDs],`,
+  `"booksInterestedIn": [objectIDs],`,
+  `"matches": [objectIDs],`,
+  `"createdAt": date,`,
+  `"updatedAt": date`,
+];
+
+//userSchema.innerHTML = <p>i am some text</p>;
+const insertCodeToUserSchema = () => {
+  userSchema.innerHTML += `<div>{</div>`;
+  for (let i = 0; i < codeArrOne.length; i++) {
+    userSchema.innerHTML += `<div class="code">${codeArrOne[i]}</div>`;
+  }
+  for (let i = 0; i < codeArrOneOptional.length; i++) {
+    userSchema.innerHTML += `<div class="code-optional">${codeArrOneOptional[i]}</div>`;
+  }
+  userSchema.innerHTML += `<div>}</div>`;
+};
+
+insertCodeToUserSchema();
