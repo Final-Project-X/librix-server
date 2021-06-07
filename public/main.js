e27be76efe5ca9b04c673b1d7f09f81ec6b049a0
Luke Dowling <lpdowling26@googlemail.com>
2021-06-07 15:28:13 +0200

    [mod] docs

--- public/main.js
+++ public/main.js
@@ -1,12 +1,32 @@
-const userSchema = document.getElementById('user-schema');
-const codeArrOne = [
-  `"username": "username",`,
-  `"email": "email",`,
-  `"passowrd": "password",`,
-  `"city": "city",`,
+const userSchema = document.getElementById('userSchema');
+const bookSchema = document.getElementById('bookSchema');
+const matchSchema = document.getElementById('matchSchema');
+
+const insertCodeToUserSchema = (schema, codeArr, codeArrOptional) => {
+  schema.innerHTML += `<div>{</div>`;
+
+  for (let i = 0; i < codeArr.length; i++) {
+    schema.innerHTML += `<div class="code">${codeArr[i]}</div>`;
+  }
+
+  if (codeArrOptional.length > 0) {
+    for (let i = 0; i < codeArrOptional.length; i++) {
+      schema.innerHTML += `<div class="code-optional">${codeArrOptional[i]}</div>`;
+    }
+  }
+
+  schema.innerHTML += `<div>}</div>`;
+};
+
+const codeArrUser = [
+  `"username": string,`,
+  `"email": string,`,
+  `"passowrd": string,`,
+  `"city": string,`,
 ];
-const codeArrOneOptional = [
-  `"aboutMe": "aboutMe",`,
+
+const codeArrUserOptional = [
+  `"aboutMe": string,`,
   `"avatar": png / defualt is provided,`,
   `"points": number / defualt 0,`,
   `"booksToOffer": [objectIDs],`,
@@ -17,16 +37,44 @@ const codeArrOneOptional = [
   `"updatedAt": date`,
 ];
 
-//userSchema.innerHTML = <p>i am some text</p>;
-const insertCodeToUserSchema = () => {
-  userSchema.innerHTML += `<div>{</div>`;
-  for (let i = 0; i < codeArrOne.length; i++) {
-    userSchema.innerHTML += `<div class="code">${codeArrOne[i]}</div>`;
-  }
-  for (let i = 0; i < codeArrOneOptional.length; i++) {
-    userSchema.innerHTML += `<div class="code-optional">${codeArrOneOptional[i]}</div>`;
-  }
-  userSchema.innerHTML += `<div>}</div>`;
-};
+const codeArrBook = [
+  `"username": string,`,
+  `"email": string,`,
+  `"passowrd": string,`,
+  `"city": string,`,
+];
 
-insertCodeToUserSchema();
+const codeArrBookOptional = [
+  `"aboutMe": string,`,
+  `"avatar": png / defualt is provided,`,
+  `"points": number / defualt 0,`,
+  `"booksToOffer": [objectIDs],`,
+  `"booksToRemember": [objectIDs],`,
+  `"booksInterestedIn": [objectIDs],`,
+  `"matches": [objectIDs],`,
+  `"createdAt": date,`,
+  `"updatedAt": date`,
+];
+
+const codeArrMatch = [
+  `"username": string,`,
+  `"email": string,`,
+  `"passowrd": string,`,
+  `"city": string,`,
+];
+
+const codeArrMatchOptional = [
+  `"aboutMe": string,`,
+  `"avatar": png / defualt is provided,`,
+  `"points": number / defualt 0,`,
+  `"booksToOffer": [objectIDs],`,
+  `"booksToRemember": [objectIDs],`,
+  `"booksInterestedIn": [objectIDs],`,
+  `"matches": [objectIDs],`,
+  `"createdAt": date,`,
+  `"updatedAt": date`,
+];
+
+insertCodeToUserSchema(userSchema, codeArrUser, codeArrUserOptional);
+insertCodeToUserSchema(bookSchema, codeArrBook, codeArrBookOptional);
+insertCodeToUserSchema(matchSchema, codeArrMatch, codeArrMatchOptional);
